77fead138329f3904d366e8d7dc9c91e9ef6d044
asivery <asivery@protonmail.com>
2024-07-25 14:34:25 +0200

    Add more devices' IDs

--- src/devices.ts
+++ src/devices.ts
@@ -1,3 +1,6 @@
 export const DeviceIds: { vendorId: number, productId: number, name: string}[] = [
-    { vendorId: 0x054c, productId: 0x01ad, name: 'Sony NW-HD1' },
+    { vendorId: 0x054c, productId: 0x01ad, name: 'Sony NW-HD1 / NW-HD2' },
+    { vendorId: 0x054c, productId: 0x0210, name: 'Sony NW-HD3' },
+    { vendorId: 0x054c, productId: 0x0233, name: 'Sony NW-HD5' },
+    { vendorId: 0x054c, productId: 0x0269, name: 'Sony NW-A3000' },
 ];
